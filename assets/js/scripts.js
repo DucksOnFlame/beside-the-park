771abd18c921c98913a55e3a26d22745185e4176
DucksOnFlame <bartlomiej.styczynski@gmail.com>
2017-03-23 18:01:41 +0100

    Publishing
    Last version. Submitting for revision.

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,14 +1,21 @@
-window.onload = appear();
+window.onload = appearOnLoad();
 
-function appear(){
+function appearOnLoad() {
+  summaryAppear();
+  iPhoneAppear();
+  mainTextAppear();
+  bottomBoxesAppear();
+}
+
+function summaryAppear(){
     var timeOut;
-    var element = document.querySelector("#js-summary__text-container");
+    var elementSummary = document.querySelector("#js-summary__text-container");
     //initial opacity
     var i = 0;
     //opacity increment
     var step = 5;
     //time waited between two opacity increments in msec
-    var speed = 80;
+    var speed = 50;
 
     timeOut = setInterval(function() {
         //get opacity in decimals
@@ -21,8 +28,90 @@ function appear(){
             return;
         }
         //modern browsers
-        element.style.opacity = opacity;
+        elementSummary.style.opacity = opacity;
         //older IE
-        element.style.filter = 'alpha(opacity=' + opacity*100 + ')';
+        elementSummary.style.filter = 'alpha(opacity=' + opacity*100 + ')';
+    }, speed);
+}
+
+function iPhoneAppear(){
+    var timeOut;
+    var elementIphone = document.querySelector("#c-iphone");
+    //initial opacity
+    var i = 0;
+    //opacity increment
+    var step = 5;
+    //time waited between two opacity increments in msec
+    var speed = 50;
+
+    timeOut = setInterval(function() {
+        //get opacity in decimals
+        var opacity = i / 100;
+        //set the next opacity step
+        i = i + step;
+        if(opacity > 1 || opacity < 0){
+            clearInterval(timeOut);
+            //if 1-opaque or 0-transparent, stop
+            return;
+        }
+        //modern browsers
+        elementIphone.style.opacity = opacity;
+        //older IE
+        elementIphone.style.filter = 'alpha(opacity=' + opacity*100 + ')';
+    }, speed);
+}
+
+function mainTextAppear(){
+    var timeOut;
+    var elementMainText = document.querySelector("#c-main-text");
+
+    //initial opacity
+    var i = 0;
+    //opacity increment
+    var step = 5;
+    //time waited between two opacity increments in msec
+    var speed = 50;
+
+    timeOut = setInterval(function() {
+        //get opacity in decimals
+        var opacity = i / 100;
+        //set the next opacity step
+        i = i + step;
+        if(opacity > 1 || opacity < 0){
+            clearInterval(timeOut);
+            //if 1-opaque or 0-transparent, stop
+            return;
+        }
+        //modern browsers
+        elementMainText.style.opacity = opacity;
+        //older IE
+        elementMainText.style.filter = 'alpha(opacity=' + opacity*100 + ')';
+    }, speed);
+}
+
+function bottomBoxesAppear(){
+    var timeOut;
+    var elementMainText = document.querySelector("#c-bottom-boxes");
+    //initial opacity
+    var i = 0;
+    //opacity increment
+    var step = 5;
+    //time waited between two opacity increments in msec
+    var speed = 50;
+
+    timeOut = setInterval(function() {
+        //get opacity in decimals
+        var opacity = i / 100;
+        //set the next opacity step
+        i = i + step;
+        if(opacity > 1 || opacity < 0){
+            clearInterval(timeOut);
+            //if 1-opaque or 0-transparent, stop
+            return;
+        }
+        //modern browsers
+        elementMainText.style.opacity = opacity;
+        //older IE
+        elementMainText.style.filter = 'alpha(opacity=' + opacity*100 + ')';
     }, speed);
 }
